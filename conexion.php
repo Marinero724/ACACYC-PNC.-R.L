2c49e3c00e2ca4f4034e7e1a3483cf88f11a1af1
Marinero724 <149142550+Marinero724@users.noreply.github.com>
2023-11-15 00:10:39 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,12 +1,12 @@
 <?php
-$servername = "roundhouse.proxy.rlwy.net"; // Cambia esto si tu base de datos está en un servidor remoto
-$username = "root"; // Cambia esto al nombre de usuario de tu base de datos
-$password = "CFf-bbbGG22A6Ddb2G1a6fa-dhG5ae3g"; // Cambia esto a tu contraseña de base de datos
-$dbname = "railway"; // Cambia esto al nombre de la base de datos que creaste
-
-
+$DB_HOST=$_ENV["DB_HOST"]; // Cambia esto si tu base de datos está en un servidor remoto
+$DB_USER=$_ENV["DB_USER"]; // Cambia esto al nombre de usuario de tu base de datos
+$DB_PASSWORD=$_ENV["DB_PASSWORD"]; // Cambia esto a tu contraseña de base de datos
+$DB_NAME=$_ENV["DB_NAME"]; // Cambia esto al nombre de la base de datos que creaste
+$DB_PORT=$_ENV["DB_PORT"];  // Cambia esto el host de la baase de datos que creaste
 // Crear una conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn =mysqli_connect("$DB_HOST", "$DB_USER", "$DB_PASSWORD", "$DB_NAME", "$DB_PORT");
+
 
 // Verificar la conexión
 if ($conn->connect_error) {
